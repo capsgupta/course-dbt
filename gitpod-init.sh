c1800812dccb6d10090db9259f094fcb6212a146
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-05-11 21:49:36 +0530

    Update gitpod-init.sh

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -2,3 +2,10 @@ echo "Running the init script"
 
 psql -f /dbt/scripts/init.sql
 psql -d dbt -f /dbt/scripts/schema.sql
+
+
+# Add jaffle shop
+wget https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_customers.csv
+wget https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_orders.csv
+wget https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_payments.csv
+psql -c 'CREATE DATABASE jaffle_shop'
