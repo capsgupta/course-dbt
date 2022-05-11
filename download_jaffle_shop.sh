c8428b405c90cdb997268be3e9754b51357eedad
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2022-05-11 21:49:52 +0530

    Delete download_jaffle_shop.sh

--- download_jaffle_shop.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_customers.csv
-wget https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_orders.csv
-wget https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_payments.csv
-psql -c 'CREATE DATABASE jaffle_shop'
